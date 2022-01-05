754d4ac0150a6618138b7bd8a2ba769ebf739f19
Karim Leon <leon.karim08@gmail.com>
2022-01-04 22:09:31 -0500

    Updated.. (deleted images, added JS file)

---
+++ main.js
@@ -0,0 +1,19 @@
+const right_arrow = document.getElementById('right_arrow');
+
+/*
+right_arrow.onmouseover = () => {
+ let right_arrowSrc = right_arrow.getAttribute('src') 
+  
+  if(right_arrowSrc === 'images/right-arrow.svg') {
+
+   right_arrow.setAttribute('src', 'images/right-arrow-hover.svg')
+  
+
+  }
+
+  else {right_arrow.setAttribute('src', 'images/right-arrow.svg')}
+}
+*/
+
+
+
