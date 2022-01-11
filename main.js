4cec81ba6bee554eb37140c827bde558c0888c15
Karim Leon <leon.karim08@gmail.com>
2022-01-10 20:07:08 -0500

    ...

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const img = document.querySelector("#intro > picture > img")
-const sideContainer2 = document.querySelector("#intro > div.side-container2")
-const dot1 = document.getElementById('dot1')
-let picSrc = document.querySelector("#intro > picture > source")
 
 
 
@@ -9,17 +5,4 @@ let picSrc = document.querySelector("#intro > picture > source")
 
 
 
-/*
-right_arrow.onmouseover = () => {
- let right_arrowSrc = right_arrow.getAttribute('src') 
-  
-  if(right_arrowSrc === 'images/right-arrow.svg') {
 
-   right_arrow.setAttribute('src', 'images/right-arrow-hover.svg')
-  
-
-  }
-
-  else {right_arrow.setAttribute('src', 'images/right-arrow.svg')}
-}
-*/
