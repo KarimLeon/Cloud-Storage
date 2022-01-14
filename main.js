cfeb22fc9a8ac13d1ff856a407629c89ec9c59e2
Karim Leon <leon.karim08@gmail.com>
2022-01-13 19:21:10 -0500

    Modified

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+'use strict'
+
+
+const dot1 = document.getElementById('dot1')
+const source = document.querySelector("#intro > picture > source")
+const img = document.querySelector("#intro > picture > img")
+let moonbig = 'images/moon-big.webp', moon1920 = 'images/moon_1920.webp';
 
 
 
-
-
-
-
-
+    
+if (img.getAttribute('src') === moonbig || moon1920) { 
+      dot1.style.backgroundColor = "white"
+  }
