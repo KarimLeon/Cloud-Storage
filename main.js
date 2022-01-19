bfaf3184f3b7048edc208980e3af8b6764a980b4
Karim Leon <leon.karim08@gmail.com>
2022-01-18 22:23:10 -0500

    Changed function declaration to Arrow function

--- main.js
+++ main.js
@@ -2,19 +2,14 @@
 const sC2 = document.querySelector("body > div.side-container2")
 const dot1 = document.getElementById('dot1')
 const img = document.querySelector("#intro > picture > img")
+let beach1920 = "images/s-1920w-1281h.webp", beach5117 = 'images/s-5117w-3414h.webp'
 
 
 
 //change img src if person clicks next(Arrow)
 
-function changeImages() {
-   img.setAttribute('src', 'images/lantern.jpg')
-}
-
-sC2.addEventListener('click', changeImages)
+sC2.addEventListener('click', () => {img.setAttribute('src', 'images/lantern.jpg')})
 
 
-
-    
-
-
+if (img.getAttribute('src') === beach1920 || beach5117) 
+     dot1.style.backgroundColor = "white"
